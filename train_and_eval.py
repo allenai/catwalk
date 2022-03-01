20f939470e0df87565d2942bd9e11d4134bd6d78
Dirk Groeneveld <dirkg@allenai.org>
2022-03-01 11:34:13 -0800

    We updated Tango

--- train_and_eval.py
+++ train_and_eval.py
@@ -30,12 +30,7 @@ def main():
         from tango import LocalWorkspace
         workspace = LocalWorkspace(args.workspace)
 
-    # TODO: dirkgr: Replace this with StepGraph.from_file() in the next version of Tango
-    params = Params.from_file(args.training_config)
-    for package_name in params.pop("include_package", []):
-        import_extra_module(package_name)
-    step_graph = StepGraph(params.pop("steps", keep_as_dict=True))
-    #step_graph = StepGraph.from_file(args.training_config)
+    step_graph = StepGraph.from_file(args.training_config)
 
     try:
         training_step = step_graph["trained_model"]

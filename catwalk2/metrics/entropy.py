05a23cdc7327b6e5cbc3e73db2e85e6e2f6f7d09
Dirk Groeneveld <dirkg@allenai.org>
2022-03-24 10:23:51 -0700

    Metrics for the perplexity task

---
+++ catwalk2/metrics/entropy.py
@@ -0,0 +1,32 @@
+import math
+from typing import Union, Optional, Any, Dict
+
+import torch
+from torchmetrics.aggregation import BaseAggregator
+
+
+class EntropyMetric(BaseAggregator):
+    def __init__(
+        self,
+        base: int = 2,  # Does anyone ever use anything but 2 here?
+        nan_strategy: Union[str, float] = "warn",
+        compute_on_step: Optional[bool] = None,
+        **kwargs: Dict[str, Any],
+    ):
+        super().__init__("entropy", [], nan_strategy, compute_on_step, **kwargs)
+        self.base = base
+        self.add_state("loglikelihood", default=torch.tensor(0.0, dtype=torch.float), dist_reduce_fx="sum")
+        self.add_state("characters", default=torch.tensor(0, dtype=torch.int), dist_reduce_fx="sum")
+
+    def update(
+        self,
+        loglikelihood: Union[float, torch.Tensor],
+        characters: Union[int, torch.Tensor]
+    ) -> None:  # type: ignore
+        loglikelihood = self._cast_and_nan_check_input(loglikelihood)
+        characters = self._cast_and_nan_check_input(characters)
+        self.loglikelihood += loglikelihood.sum()
+        self.characters += characters.sum()
+
+    def compute(self) -> torch.Tensor:
+        return -(self.loglikelihood / self.characters) / math.log(self.base)
---
+++ catwalk2/metrics/perplexity.py
@@ -0,0 +1,30 @@
+from typing import Union, Optional, Dict, Any
+
+import torch
+from torchmetrics.aggregation import BaseAggregator
+
+
+class PerplexityMetric(BaseAggregator):
+    def __init__(
+        self,
+        nan_strategy: Union[str, float] = "warn",
+        compute_on_step: Optional[bool] = None,
+        **kwargs: Dict[str, Any],
+    ):
+        super().__init__("perplexity", [], nan_strategy, compute_on_step, **kwargs)
+        self.add_state("loglikelihood", default=torch.tensor(0.0, dtype=torch.float), dist_reduce_fx="sum")
+        self.add_state("num_tokens", default=torch.tensor(0, dtype=torch.int), dist_reduce_fx="sum")
+
+    def update(
+        self,
+        loglikelihood: Union[float, torch.Tensor],
+        num_tokens: Union[int, torch.Tensor]
+    ) -> None:  # type: ignore
+        loglikelihood = self._cast_and_nan_check_input(loglikelihood)
+        num_tokens = self._cast_and_nan_check_input(num_tokens)
+        self.loglikelihood += loglikelihood.sum()
+        self.num_tokens += num_tokens.sum()
+
+    def compute(self) -> torch.Tensor:
+        return torch.exp(-self.loglikelihood / self.num_tokens)
+

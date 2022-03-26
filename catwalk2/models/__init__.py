11eef1fae2a3cf514b0774d09d394a687d5700d5
Dirk Groeneveld <dirkg@allenai.org>
2022-03-25 23:40:40 -0700

    Adds T5 and RTE

--- catwalk2/models/__init__.py
+++ catwalk2/models/__init__.py
@@ -1,8 +1,14 @@
 from catwalk2.models.gpt import GPTModel
 from catwalk2.models.huggingface import HFAutoModel
+from catwalk2.models.t5 import T5Model
 
 MODELS = {
     "gpt2": GPTModel("gpt2"),
     "bert-base-uncased": HFAutoModel("bert-base-uncased"),
-    "bert-base-cased": HFAutoModel("bert-base-cased")
+    "bert-base-cased": HFAutoModel("bert-base-cased"),
+    "t5-base": T5Model("t5-base"),
+    "t5-large": T5Model("t5-large"),
+    "t5-small": T5Model("t5-small"),
+    "t5-3b": T5Model("t5-3b"),
+    "t5-11b": T5Model("t5-11b")
 }
---
+++ catwalk2/models/t5.py
@@ -0,0 +1,62 @@
+from typing import Sequence, Dict, Any, Iterator
+
+import more_itertools
+import torch
+from tango.common import Tqdm
+from tango.common.sequences import MappedSequence
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+from catwalk2.task import Task, InstanceFormat
+from catwalk2.model import Model, UnsupportedTaskError
+
+_true_tensor = torch.tensor([True])
+_false_tensor = torch.tensor([False])
+
+
+@Model.register("catwalk::t5")
+class T5Model(Model):
+    def __init__(self, pretrained_model_name_or_path: str):
+        self.pretrained_model_name_or_path = pretrained_model_name_or_path
+
+    def predict(
+        self,
+        task: Task,
+        instances: Sequence[Dict[str, Any]],
+        *,
+        batch_size: int = 32
+    ) -> Iterator[Dict[str, Any]]:
+        if not task.has_instance_conversion(InstanceFormat.T5_PROMPT):
+            raise UnsupportedTaskError(self, task)
+        prompts = MappedSequence(task.instance_conversions[InstanceFormat.T5_PROMPT], instances)
+
+        model = AutoModelForSeq2SeqLM.from_pretrained(self.pretrained_model_name_or_path).eval()
+        tokenizer = AutoTokenizer.from_pretrained(self.pretrained_model_name_or_path)
+
+        def strip_special_tokens(t: torch.Tensor) -> torch.Tensor:
+            # amazing that torch has no capability for this
+            start = 0
+            while start < len(t) and int(t[start]) in {0, tokenizer.eos_token_id, tokenizer.pad_token_id}:
+                start += 1
+            end = len(t)
+            while int(t[end - 1]) in {0, tokenizer.eos_token_id, tokenizer.pad_token_id} and end > start:
+                end -= 1
+            return t[start:end]
+
+        with torch.inference_mode():
+            for batch in more_itertools.chunked(Tqdm.tqdm(prompts, desc="Processing instances"), batch_size):
+                model_input = tokenizer(
+                    [i[0] for i in batch],
+                    padding=True,
+                    truncation="only_first",
+                    return_tensors="pt",
+                    pad_to_multiple_of=8)
+                model_output = model.generate(**model_input)
+                model_output = [strip_special_tokens(t) for t in model_output]
+                model_output = tokenizer.batch_decode(model_output, clean_up_tokenization_spaces=True)
+                for target, prediction in zip(batch, model_output):
+                    target = target[1]
+                    yield {
+                        "acc": (torch.Tensor([target == prediction]), _true_tensor),
+                        "bleu": ([prediction], [[target]]),
+                        "rouge": ([prediction], [[target]])
+                    }

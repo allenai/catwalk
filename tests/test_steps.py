7b918563e9e83687128e41e7e03d1d1308b6241f
Dirk Groeneveld <dirkg@allenai.org>
2022-04-28 17:28:32 -0700

    Brings the tests up to date

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -35,19 +35,11 @@ task_names = [
     "wsc",
 ]
 
-task_names.remove("pubmedqa")
-task_names.append(
-    pytest.param(
-        "pubmedqa",
-        marks=pytest.mark.xfail(reason="This task is broken in the latest version of the Eleuther framework.")))
-
 
 @pytest.mark.parametrize("task_name", task_names)
 @pytest.mark.parametrize("model_name", ["eai::tiny-gpt2"])
 def test_task_eval(task_name: str, model_name: str):
-    tasks_without_val = {"prost", "webqs"}
-    split = "test" if task_name in tasks_without_val else "validation"
-    predict_step = PredictStep(model=model_name, task=task_name, limit=10, split=split)
+    predict_step = PredictStep(model=model_name, task=task_name, limit=10)
     metrics_step = CalculateMetricsStep(model=model_name, task=task_name, predictions=predict_step)
     result = metrics_step.result()
     assert result is not None

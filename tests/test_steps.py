317965ce5d82450371a39e2c3d21b3a766e65f90
Dirk Groeneveld <dirkg@allenai.org>
2022-05-06 18:19:44 -0700

    Run fewer tests

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -2,36 +2,27 @@ import pytest
 
 from catwalk.steps import PredictStep, CalculateMetricsStep
 
-# This is a list of Tasks Iz has deemed important.
 task_names = [
     "arc_challenge",
-    "arc_easy",
     "boolq",
     "copa",
     "headqa",
     "hellaswag",
     "lambada",
     "logiqa",
-    "mathqa",
     "mc_taco",
     "mrpc",
     "multirc",
     "openbookqa",
     "piqa",
-    "piqa",
-    "prost",
-    "pubmedqa",
     "qnli",
     "qqp",
-    "race",
     "rte",
     "sciq",
     "sst",
-    "triviaqa",
     "webqs",
     "wic",
     "winogrande",
-    "wnli",
     "wsc",
 ]
 

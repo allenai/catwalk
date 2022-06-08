735ca3dbc39519fdfc872a2a03f99558acb946a6
Dirk Groeneveld <dirkg@allenai.org>
2022-06-07 18:20:27 -0700

    Adding release machinery

---
+++ scripts/release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+TAG=$(python -c 'from catwalk.version import VERSION; print("v" + VERSION)')
+
+read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
+
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+    python scripts/prepare_changelog.py
+    git add -A
+    git commit -m "Prepare for release $TAG" || true && git push
+    echo "Creating new git tag $TAG"
+    git tag "$TAG" -m "$TAG"
+    git push --tags
+else
+    echo "Cancelled"
+    exit 1
+fi

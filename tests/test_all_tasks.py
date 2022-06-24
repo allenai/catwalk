ba2dc778415a76cb53e31ec56f12b6603de31433
Dirk Groeneveld <dirkg@allenai.org>
2022-06-24 13:16:02 -0700

    Bring back P3 testing

--- tests/test_all_tasks.py
+++ tests/test_all_tasks.py
@@ -6,9 +6,9 @@ import pytest
 import catwalk.tasks
 import catwalk.models
 
-# There are too many P3 tasks, and picking one downloads all of them, so we have to skip them.
+# There are too many P3 tasks, so we just pick one.
 task_names = [task for task in catwalk.tasks.TASKS.keys() if not task.startswith("p3::")]
-#task_names.remove("triviaqa")   # triviaqa is huge
+task_names.append("p3::wiki_qa_Is_This_True_")
 
 
 @pytest.mark.parametrize("task_name", task_names)

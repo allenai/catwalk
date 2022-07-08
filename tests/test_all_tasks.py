c2fa82a4abdc42bc3c11c264ad74f50ac0ad3d32
Anas Awadalla <anasa2@uw.edu>
2022-07-07 19:54:25 -0700

    removed EleutherTask debug statement

--- tests/test_all_tasks.py
+++ tests/test_all_tasks.py
@@ -7,9 +7,8 @@ import catwalk.tasks
 import catwalk.models
 
 # There are too many P3 tasks, so we just pick one.
-task_names = [task for task in catwalk.tasks.TASKS.keys() if not task.startswith("p3::") and isinstance(catwalk.tasks.TASKS[task], catwalk.tasks.EleutherTask)]
+task_names = [task for task in catwalk.tasks.TASKS.keys() if not task.startswith("p3::")]
 task_names.insert(0, "p3::wiki_qa_Is_This_True_")
-
 
 @pytest.mark.parametrize("task_name", task_names)
 def test_task(task_name: str):

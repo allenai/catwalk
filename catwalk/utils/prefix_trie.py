2b993e83ed61d04ee745a78b1e779ee9e27594a6
jagnusson <magnusson.i@northeastern.edu>
2022-07-22 16:48:51 -0700

    relax min prefix requirement

--- catwalk/utils/prefix_trie.py
+++ catwalk/utils/prefix_trie.py
@@ -3,9 +3,22 @@ from tango.common import Tqdm
 
 
 class PrefixTrie():
-    def __init__(self, sequences: Sequence[Sequence[int]], minimum_prefix: int = 10):
+    def __init__(self, sequences: Sequence[Sequence[int]], track_after_depth: int = 10):
+        """
+        Returns a PrefixTrie for ordering examples by common prefixes
+
+        # Parameters
+
+        sequences : `Sequence[Sequence[int]]`
+            Sequences of tokens to add to the add to the Trie
+        track_after_depth : `int`
+            Only record sequence indices in nodes at or below this depth. This allows distinct
+            sequences that coincidentally start with the first few tokens as another sequence
+            not to be dropped from this barely overlapping prefix. Sequences shorter than the
+            minimum depth will only have their index recorded in their final node.
+        """
         self.root = PrefixTrieNode()
-        self.minimum_prefix = minimum_prefix
+        self.track_after_depth = track_after_depth
         self.nodes: List['PrefixTrieNode'] = []
         for i, sequence in Tqdm.tqdm(enumerate(sequences), desc="Building PrefixTrie for caching", total=len(sequences)):
             self.add_sequence(sequence=sequence, index=i)
@@ -16,14 +29,14 @@ class PrefixTrie():
                 node.lengths_covered = []
     
     def add_sequence(self, sequence: Sequence[int], index: int):
-        assert len(sequence) >= self.minimum_prefix, f"sequence with len {len(sequence)} too small for PrefixTrie with minimum_prefix {self.minimum_prefix}"
+        seq_len = len(sequence)
         current_node = self.root
         for token_idx, token in enumerate(sequence):
             if token not in current_node.children:
                 current_node.children[token] = PrefixTrieNode(parent=current_node, token=token)
                 self.nodes.append(current_node.children[token])
             current_node = current_node.children[token]
-            if token_idx + 1 >= self.minimum_prefix:
+            if (token_idx + 1 >= self.track_after_depth) or (token_idx + 1 >= seq_len):
                 current_node.indices.append(index)
                 current_node.lengths_covered.append(token_idx + 1)
     
@@ -65,7 +78,7 @@ class PrefixTrieNode():
 
 if __name__ == "__main__":
     sequences = [[1,2,3],[2,3,4],[1,2,3,4]]
-    trie = PrefixTrie(sequences, minimum_prefix=1)
+    trie = PrefixTrie(sequences, track_after_depth=1)
     leaves = trie.get_leaf_nodes()
     assert leaves[0].get_sequence() == [2,3,4]
     assert leaves[1].get_sequence() == [1,2,3,4]

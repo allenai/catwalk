441ffa18cb108936b830b40728057391616e00da
Dirk Groeneveld <dirkg@allenai.org>
2022-09-26 22:01:35 -0700

    Gotta use partial, not nested functions, because Python is an insane language

--- catwalk/tasks/promptsource.py
+++ catwalk/tasks/promptsource.py
@@ -1,3 +1,4 @@
+import functools
 from typing import Dict, Any, Optional, Sequence
 
 from promptsource.templates import DatasetTemplates, TemplateCollection
@@ -14,39 +15,43 @@ def _index_case_insensitive(sequence: Sequence[str], value: str) -> int:
 
 
 def promptsource_conversion(
-    *,
     dataset_templates: DatasetTemplates,
 ) -> InstanceConversion:
-    def convert(instance: Dict[str, Any]) -> Dict[str, RankClassificationInstance]:
-        prompts = {
-            template_name: (
-                dataset_templates[template_name].apply(instance),
-                dataset_templates[template_name].get_answer_choices_list(instance)
-            ) for template_name in dataset_templates.all_template_names
-        }
-        # filter out invalid prompts
-        prompts = {
-            template_name: (prompt, answer_choices)
-            for template_name, (prompt, answer_choices) in prompts.items()
-            if len(prompt) == 2
-        }
-        # assert that there is only one answer
-        assert all(
-            (
-                (answer_choices is None) or
-                (correct_answer is None) or
-                (len(correct_answer) == 1)
-            ) for (prompt, correct_answer), answer_choices in prompts.values()
-        )
-        # package up as a RankClassificationInstance
-        return {
-            template_name: RankClassificationInstance(
-                [(prompt, choice) for choice in answer_choices],
-                _index_case_insensitive(answer_choices, correct_answer[0]) if correct_answer is not None else None
-            ) for template_name, ((prompt, correct_answer), answer_choices) in prompts.items() if answer_choices is not None
-        }
+    return functools.partial(promptsource_convert, dataset_templates=dataset_templates)
 
-    return convert
+
+def promptsource_convert(
+    instance: Dict[str, Any],
+    *,
+    dataset_templates: DatasetTemplates
+) -> Dict[str, RankClassificationInstance]:
+    prompts = {
+        template_name: (
+            dataset_templates[template_name].apply(instance),
+            dataset_templates[template_name].get_answer_choices_list(instance)
+        ) for template_name in dataset_templates.all_template_names
+    }
+    # filter out invalid prompts
+    prompts = {
+        template_name: (prompt, answer_choices)
+        for template_name, (prompt, answer_choices) in prompts.items()
+        if len(prompt) == 2
+    }
+    # assert that there is only one answer
+    assert all(
+        (
+            (answer_choices is None) or
+            (correct_answer is None) or
+            (len(correct_answer) == 1)
+        ) for (prompt, correct_answer), answer_choices in prompts.values()
+    )
+    # package up as a RankClassificationInstance
+    return {
+        template_name: RankClassificationInstance(
+            [(prompt, choice) for choice in answer_choices],
+            _index_case_insensitive(answer_choices, correct_answer[0]) if correct_answer is not None else None
+        ) for template_name, ((prompt, correct_answer), answer_choices) in prompts.items() if answer_choices is not None
+    }
 
 
 def promptsource_templates_for_task(task: Task) -> Optional[DatasetTemplates]:

5929232e5e55d315d3a011135aaf834406238a90
Dirk Groeneveld <dirkg@allenai.org>
2022-12-07 09:57:35 +0400

    Adds tool to print results

---
+++ experiments/everything/print_results.py
@@ -0,0 +1,28 @@
+import argparse
+
+from tango import Workspace
+from tango.common.logging import initialize_logging
+
+
+def main():
+    initialize_logging(log_level="WARNING")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--run', '-r', type=str, required=True)
+    parser.add_argument('--step', '-s', type=str, default="tabulate")
+    parser.add_argument(
+        '-d', '-w',
+        type=str,
+        default="beaker://ai2/task-complexity",
+        metavar="workspace",
+        dest="workspace",
+        help="the Tango workspace with the cache")
+    args = parser.parse_args()
+
+    workspace = Workspace.from_url(args.workspace)
+    r = workspace.step_result_for_run(args.run, args.step)
+    print("\n".join(r))
+
+
+if __name__ == "__main__":
+    main()

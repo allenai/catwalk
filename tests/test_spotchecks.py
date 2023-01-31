a8ff413d9cc7c2b19330d29afcd26feceebdfc29
Dirk Groeneveld <dirkg@allenai.org>
2023-01-30 16:42:22 -0800

    Adds a test for some tasks I recently fixed

--- tests/test_spotchecks.py
+++ tests/test_spotchecks.py
@@ -1,6 +1,9 @@
 import sys
 
+import pytest
+
 import catwalk.__main__
+from catwalk.steps import PredictStep, CalculateMetricsStep
 
 
 def test_squad():
@@ -11,3 +14,12 @@ def test_squad():
         "--limit", "100"
     ])
     catwalk.__main__.main(args)
+
+
+@pytest.mark.parametrize("task", ["mnli", "cola", "rte"])
+def test_gpt2_performance(task: str):
+    model = "rc::gpt2"
+    predictions = PredictStep(model=model, task=task, limit=100)
+    metrics = CalculateMetricsStep(model=model, task=task, predictions=predictions)
+    results = metrics.result()
+    assert results['relative_improvement'] > 0
